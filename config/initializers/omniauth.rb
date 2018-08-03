e9c6de5011aa1c9e6dd4284b0f991c70b90db99b
CatsAreMLG <joshuadgon@gmail.com>
2018-08-03 02:10:51 -0500

    Added Facebook sign in function

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '193266678211520', 'b886974d52b031ea4deef4884ed2cee0'
+end
