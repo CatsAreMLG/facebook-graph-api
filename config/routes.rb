27fd7a5ee6d9c2c26c6de4ed2023b48b0cb6c351
CatsAreMLG <joshuadgon@gmail.com>
2018-08-03 02:36:05 -0500

    Added Facebook redirect & integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'serrions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
