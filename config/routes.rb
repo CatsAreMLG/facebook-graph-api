c118a15459c8c18cb1c28f8cee40299245e9afe8
CatsAreMLG <joshuadgon@gmail.com>
2018-08-03 02:41:47 -0500

    Fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  match 'auth/:provider/callback', to: 'serrions#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
   get 'welcome/index'
   root 'welcome#index'
