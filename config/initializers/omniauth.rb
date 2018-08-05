0282f2d8593fd0449313b9218d306b35700c7937
CatsAreMLG <joshuadgon@gmail.com>
2018-08-04 20:59:01 -0500

    Testing Firgaro fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Figaro.env.FACEBOOK_APP_ID, Figaro.env.FACEBOOK_SECRET, scope: 'email', info_fields: 'email,name'
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], scope: 'email', info_fields: 'email,name'
 end
