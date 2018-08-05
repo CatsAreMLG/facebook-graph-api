66a5edbc6dc3e446aef3a2dbf49e8650759da54f
CatsAreMLG <joshuadgon@gmail.com>
2018-08-04 21:28:08 -0500

    Testing omniauth fix in heroku

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'omniauth-facebook'
 gem 'figaro'
+gem 'koala'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,13 @@
 class User < ApplicationRecord
-  # def self.from_omniauth(auth)
-  #   where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-  #     user.provider = auth.provider
-  #     user.uid = auth.uid
-  #     user.name = auth.info.name
-  #     user.oauth_token = auth.credentials.token
-  #     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-  #     user.save!
-  #   end
-  # end
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid      = auth.uid
       user.name     = auth.info.name
       user.save
     end
+  end
+  def facebook
+    @facebook ||= Koala::Facebook::API.new(oauth_token)
   end
 end
