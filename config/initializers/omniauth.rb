b5cdfb41427d6ae2a9c6de57bb36daa9018c648a
CatsAreMLG <joshuadgon@gmail.com>
2018-08-04 20:41:42 -0500

    Added Figaro

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '193266678211520', 'b886974d52b031ea4deef4884ed2cee0', scope: 'email', info_fields: 'email,name'
+  provider :facebook, Figaro.env.FACEBOOK_APP_ID, Figaro.env.FACEBOOK_SECRET, scope: 'email', info_fields: 'email,name'
 end
