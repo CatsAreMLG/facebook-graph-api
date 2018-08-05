61f71d9616b775aaf2aff03ed2238167283de4dd
CatsAreMLG <joshuadgon@gmail.com>
2018-08-04 22:09:26 -0500

    Testing omniauth fix in heroku.6

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], scope:  'user_photos', 'user_posts', 'user_videos', 'user_link'
 end
